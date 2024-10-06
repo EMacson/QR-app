3affc1f50b30548cb80fdb7cc3b7a1492d776d90
ethan m <48036998+EMacson@users.noreply.github.com>
2024-10-06 15:22:31 -0700

    qr code is displayed after submitting

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@ const qr = document.getElementById('qrcode');
 const onGenerateSubmit = (e) => {
     e.preventDefault();
 
+    clearUI();
+
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
 
     if(url === '') {
         alert('Please enter a URL');
     } else {
-        console.log('generate qr');
         generateQRCode(url, size);
+
+        
     }
 };
 
@@ -21,6 +24,20 @@ const generateQRCode = (url, size) => {
         width: size,
         height: size,
     });
+};
+
+const clearUI = () => {
+    qr.innerHTML = '';
+};
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated').appendChild(link);
 };
 
 form.addEventListener('submit', onGenerateSubmit);
